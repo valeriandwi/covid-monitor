701ce2fb41e7f8266d89502c798a8c3018d4936e
Valerian Dwi Purnomo <valerian.dwi.p@gmail.com>
2020-09-19 22:00:44 +0700

    First Commit

---
+++ (public) build/precache-manifest.1e7b24284e19b04650eb1140798726c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33328cc82ca8711ec179c07adfd74c76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea08caae22b40bcbd00c",
+    "url": "/static/css/main.fed3ab96.chunk.css"
+  },
+  {
+    "revision": "9c96f526bc4e1cd47439",
+    "url": "/static/js/2.ead2aaab.chunk.js"
+  },
+  {
+    "revision": "02f397bcddefbac9b5b5ade8a9cc15f2",
+    "url": "/static/js/2.ead2aaab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea08caae22b40bcbd00c",
+    "url": "/static/js/main.a6c41351.chunk.js"
+  },
+  {
+    "revision": "c6f9b8b110b31b5800cb",
+    "url": "/static/js/runtime-main.0cc35614.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chart from 'react-apexcharts';
+import axios from 'axios';
+import moment from 'moment';
+import {Typography} from '@material-ui/core';
 
 function App() {
+  const [chartData,setChartData] = useState([]);
+  useEffect(() => {
+    axios.get("https://indonesia-covid-19.mathdro.id/api/harian")
+    .then(res => {
+      setChartData(res.data.data.filter(data => data.jumlahKasusKumulatif !== null))
+    })
+    .catch(err => alert(err.response.data));
+  }, []);
+
+  const options = {
+    chart: {
+      type: 'area',
+      stacked: false,
+        height: 350,
+      zoom: {
+          type: 'x',
+          enabled: true,
+          autoScaleYaxis: true
+      },
+      toolbar: {
+          autoSelected: 'zoom'
+      },
+    },
+    dataLabels: {
+      enabled: false
+    },
+    markers: {
+      size: 0,
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shadeIntensity: 1,
+        inverseColors: false,
+        opacityFrom: 0.5,
+        opacityTo: 0,
+        stops: [0, 90, 100]
+      },
+    },
+    yaxis: {
+      title: {
+        text: 'Jumlah Kasus Baru',
+        style: {
+          color: "#fff"
+        }
+      },
+      labels: {
+        show: true,
+        style: {
+          colors: "#fff"
+        }
+      },
+      axisTicks: {
+        color: "#fff"
+      }
+    },
+    xaxis: {
+      type: 'datetime',
+      categories: chartData.map(res => res.tanggal),
+      labels: {
+        formatter: function (value, timestamp){
+          return moment(new Date(value)).format("DD MMM") // The formatter function overrides format property
+        }, 
+        style: {
+          colors: "#fff"
+        }
+      }
+    },
+  }
+  
+  const series = [{
+    name: 'Jumlah Kasus Baru',
+    data: chartData.map(res => res.jumlahKasusBaruperHari)
+  }]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <Typography variant="h5" style={{color:"#fff"}}>Perkembangan Kasus Baru COVID-19</Typography>
+          <Chart
+            options={options}
+            series={series}
+            type="area"
+            width="800px"
+          />
+          <Typography variant="body2" style={{color:"#fff",marginTop:"20px"}}>&#169; VDP</Typography>
+        </div>
       </header>
     </div>
   );
